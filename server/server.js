fe5eb1bfdf8a0934efe62b154a48cf3a89ed62e4
Wechuli <wechulipaul@yahoo.com>
2020-02-14 23:30:31 +0300

    adding mongodb connection logic and user routes

--- server/server.js
+++ server/server.js
@@ -5,16 +5,37 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 
+// import routes
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 // middleware
 app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// mongoose connection setup
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.info("Database successfully connected");
+  })
+  .catch(error => console.error(error));
+
 // routes
+
+app.use("/api/users", userRoutes);
 
 // port
 const PORT = process.env.PORT || 3002;
@@ -22,5 +43,4 @@ const PORT = process.env.PORT || 3002;
 // listen
 app.listen(PORT, () => {
   console.info(`Server running at ${PORT}`);
-  console.log(process.env.JWT_SECRET);
 });
