e7c19e4a5d9c20c9ae11d9a17e9e7fd3b1488295
Wechuli <wechulipaul@yahoo.com>
2020-02-16 17:02:13 +0300

    product route controller

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth");
+const { createNewBrand } = require("../controllers/brandControllers");
 
 const router = express.Router();
-
 
 //============================================
 //              BRANDS
 //=============================================
 
 // create a new brand
-router.post("/brand",authMiddleware,)
+router.post("/brand", authMiddleware, createNewBrand);
 module.exports = router;
